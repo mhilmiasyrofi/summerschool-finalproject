a37d02a961edcdacc9738448245ce599f7b9213d
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2018-07-13 05:06:05 +0200

    calculate averege rating for each category

--- Main.R
+++ Main.R
@@ -1,7 +1,8 @@
-install.packages("rjson")
-install.packages("jsonlite")
-install.packages("quanteda")
-install.packages("wordcloud")
+## Required Packages
+# install.packages("rjson")
+# install.packages("jsonlite")
+# install.packages("quanteda")
+
 library("rjson")
 library("jsonlite")
 library("quanteda")
@@ -13,7 +14,7 @@ conn2 <- file(fileReview,open="r")
 df1 = stream_in(conn2)
 close(conn2)
 
-View(head(df1))
+# View(head(df1))
 
 fileName = "data/CellPhones.strict"
 
@@ -24,7 +25,7 @@ for (i in 1:length(linn)){
   if (i < 50000) {
     js = fromJSON(linn[i])
     if (!is.null(js$brand)) {
-      if (js$brand == "Samsung" || js$brand == "Apple" || js$brand == "Nokia" ) {
+      if (js$brand == "Samsung" | js$brand == "Apple" | js$brand == "Nokia" ) {
         if (first) {
           # print(js)
           df2<-data.frame(js$asin, js$brand)
@@ -42,21 +43,37 @@ for (i in 1:length(linn)){
 }
 close(conn)
 
-
-View(head(df1))
-View(head(df2))
+# View(head(df1))
+# View(head(df2))
 
 df = merge(df1, df2)
-View(head(df))
+# View(head(df))
 
 df = data.frame(ID = df$asin, Review = df$reviewText, Brand = df$Brand, Rating = df$overall)
-View((df))
-View(head(df))
 
-test = "akk"
-val = "aku dan kamu"
+review = tokens(as.character(df$Review), remove_number = T, remove_symbols = TRUE)
 
-grepl(test, val)
+dtm <- dfm(review, tolower = TRUE, stem = TRUE,   # create dtm with preprocessing
+           remove_punct = TRUE, remove = stopwords(source = "smart")) 
+
+topfeatures(dtm, 10)
+
+doc_freq <- docfreq(dtm)         # document frequency per term (column) 
+dtm <- dtm[, doc_freq >= 30]      # select terms with doc_freq >= 2 
+dtm <- dfm_weight(dtm, "tfidf")  # weight the features using tf-idf 
+## Warning: scheme = "tfidf" is deprecated; use dfm_tfidf(x) instead
+# head(dtm)
+
+col <- sapply(seq(0.5, 2, 0.5), function(x) adjustcolor("#1F78B4", x))
+
+# first we visualize topic model for the reivew to get the main topics
+# unforunately it is not good enough
+# we decide to make own category
+textplot_wordcloud(dtm, color = col)
+
+
+# ===================================================
+# Make category
 
 category = c("service", "design", "function", "package", "quality")
 
@@ -76,32 +93,38 @@ for (x in df$Review) {
   cats[length(cats)+1] = cat
 }
 
-print(length(cats))
-
-# View(head(cats))
-
 df$Category = cats
 
-# View(head(df))
-View(df)
+dfSamsung = df[df$Brand == "Samsung",]
+dfApple = df[df$Brand == "Apple",]
+dfNokia = df[df$Brand == "Nokia",]
 
-dtm <- dfm(as.character(df$Review), tolower = TRUE, stem = TRUE,   # create dtm with preprocessing
-           remove_punct = TRUE, remove = stopwords(source = "smart")) 
+# Rating per brands
+meanRatingSamsung = mean(dfSamsung$Rating)
+meanRatingApple = mean(dfApple$Rating)
+meanRatingNokia = mean(dfNokia$Rating)
 
-topfeatures(dtm, 10)
+# Calculate average value of rating from each category for each brands
+ratesSamsung = c()
+for (c in category) {
+  ratesSamsung[length(ratesSamsung)+1] = round(mean(dfSamsung$Rating[dfSamsung$Category == c], na.rm = T),2)
+}
 
-?dfm
+ratesApple = c()
+for (c in category) {
+  ratesApple[length(ratesApple)+1] = round(mean(dfApple$Rating[dfApple$Category == c], na.rm = T), 2)
+}
 
-doc_freq <- docfreq(dtm)         # document frequency per term (column) 
-dtm <- dtm[, doc_freq >= 40]      # select terms with doc_freq >= 2 
-dtm <- dfm_weight(dtm, "tfidf")  # weight the features using tf-idf 
-## Warning: scheme = "tfidf" is deprecated; use dfm_tfidf(x) instead
-# head(dtm)
+ratesNokia = c()
+for (c in category) {
+  ratesNokia[length(ratesNokia)+1] = round(mean(dfNokia$Rating[dfNokia$Category == c], na.rm = T), 2)
+}
 
-col <- sapply(seq(0.5, 1, 0.5), function(x) adjustcolor("#1F78B4", x))
+print(category)
+print(ratesSamsung)
+print(ratesApple)
+print(ratesNokia)
 
-textplot_wordcloud(dtm, color = col)
+summary = data.frame(category, ratesSamsung, ratesApple, ratesNokia)
 
-?wordcloud
-
-
+View(summary)
