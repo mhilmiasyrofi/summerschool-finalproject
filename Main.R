375f28e812945bd02180c008c08b3382e96b3246
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2018-07-13 05:33:13 +0200

    exctract key text from sentence

--- Main.R
+++ Main.R
@@ -1,4 +1,18 @@
+# ======================
 ## Required Packages
+# ======================
+
+## Library for NLP to extract key text from sentence
+# install.packages("rJava")
+# install.packages("openNLP")
+# install.packages("NLP")
+
+library(rJava)
+require("openNLP")
+require("NLP")
+
+
+## Library to get some insight from Amazon Data
 # install.packages("rjson")
 # install.packages("jsonlite")
 # install.packages("quanteda")
@@ -8,6 +22,57 @@ library("jsonlite")
 library("quanteda")
 library("wordcloud")
 
+# =========================================
+## Part 1 - Extract Key Text from Sentence
+# =========================================
+
+## Function to get the key text from sentence
+extractChunks <- function(x) {
+  
+  x <- as.String(x)
+  wordAnnotation <- annotate(x, list(Maxent_Sent_Token_Annotator(), Maxent_Word_Token_Annotator()))
+  POSAnnotation <- annotate(x, Maxent_POS_Tag_Annotator(), wordAnnotation)
+  POSwords <- subset(POSAnnotation, type == "word")
+  tags <- sapply(POSwords$features, '[[', "POS")
+  tokenizedAndTagged <- data.frame(Tokens = x[POSwords], Tags = tags)
+  
+  tokenizedAndTagged$Tags_mod = grepl("NN|JJ", tokenizedAndTagged$Tags)
+  chunk = vector()
+  
+  chunk[1] = as.numeric(tokenizedAndTagged$Tags_mod[1])
+  
+  for (i in 2:nrow(tokenizedAndTagged)) {
+    
+    if(!tokenizedAndTagged$Tags_mod[i]) {
+      chunk[i] = 0
+    } else if (tokenizedAndTagged$Tags_mod[i] == tokenizedAndTagged$Tags_mod[i-1]) {
+      chunk[i] = chunk[i-1]
+    } else {
+      chunk[i] = max(chunk) + 1
+    }
+    
+  }
+  
+  text_chunk <- split(as.character(tokenizedAndTagged$Tokens), chunk)
+  tag_pattern <- split(as.character(tokenizedAndTagged$Tags), chunk)
+  names(text_chunk) <- sapply(tag_pattern, function(x) paste(x, collapse = "-"))
+  
+  # Extract chunks matching pattern
+  res = text_chunk[grepl("JJ-NN|NN.-NN", names(text_chunk))]
+  res = sapply(res, function(x) paste(x, collapse =  " "))
+  return(res)
+  
+  gc()
+  
+}
+
+main = extractChunks("Complimentary gym access for two for the length of stay $12 value per person per day")
+
+
+
+# ===============================================
+## Part 2 - Finding The Insight From Amazon Data
+# ===============================================
 
 fileReview = "data/Cell_Phones_and_Accessories_5.json"
 conn2 <- file(fileReview,open="r")
@@ -22,7 +87,7 @@ conn <- file(fileName,open="r")
 linn <-readLines(conn)
 first = TRUE
 for (i in 1:length(linn)){
-  if (i < 50000) {
+  if (i < 70000) {
     js = fromJSON(linn[i])
     if (!is.null(js$brand)) {
       if (js$brand == "Samsung" | js$brand == "Apple" | js$brand == "Nokia" ) {
